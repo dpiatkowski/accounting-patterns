5ce30ce5640970c3865b954d00658555e091a233
dpiatkowski <damian.dem@gmail.com>
2024-01-30 19:39:47 +0100

    better code coverage

--- src/events_test.ts
+++ src/events_test.ts
@@ -1,8 +1,12 @@
-import { assertThrows } from "assert/mod.ts";
+import { assert, assertThrows } from "assert/mod.ts";
 import { Customer } from "./customer.ts";
 import { TaxEvent, UsageAccountingEvent } from "./events.ts";
 import { Money } from "./money.ts";
 import { ServiceAgreement } from "./serviceAgreement.ts";
+import {
+  AmmountFormulaPostingRule,
+  MultiplyByRatePostingRule,
+} from "./postingRules.ts";
 
 Deno.test("Cannot taxate tax event", () => {
   const currency = "PLN";
@@ -19,5 +23,52 @@ Deno.test("Cannot taxate tax event", () => {
 
   assertThrows(() => {
     new TaxEvent(taxEvent, new Money(10, currency));
+  });
+});
+
+Deno.test("Cannot process event without rule in service agreement", () => {
+  const currency = "PLN";
+  const currentTime = Temporal.Now.instant();
+  const serviceAgreement = new ServiceAgreement(420);
+
+  const event = new UsageAccountingEvent(
+    new Money(500, currency),
+    currentTime,
+    currentTime,
+    new Customer("John", serviceAgreement),
+  );
+
+  assertThrows(() => {
+    event.process();
+  });
+});
+
+Deno.test("Cannot process event twice", () => {
+  const currency = "PLN";
+  const currentTime = Temporal.Now.instant();
+
+  const serviceAgreement = new ServiceAgreement(420);
+  serviceAgreement.addPostingRule(
+    "Usage",
+    new MultiplyByRatePostingRule("BaseUsage"),
+    Temporal.Instant.from("2023-04-01T01:00:00Z"),
+  );
+  serviceAgreement.addPostingRule(
+    "Tax",
+    new AmmountFormulaPostingRule(0.055, Money.zero(currency), "Tax"),
+    Temporal.Instant.from("2023-04-01T01:00:00Z"),
+  );
+
+  const event = new UsageAccountingEvent(
+    new Money(500, currency),
+    currentTime,
+    currentTime,
+    new Customer("John", serviceAgreement),
+  );
+
+  event.process();
+
+  assertThrows(() => {
+    event.process();
   });
 });
